d12f09952859e6465e4d854bc1ecc4239178e475
lkbrian <lkbrian742@gmail.com>
2023-12-19 13:02:12 +0300

    completed assignments

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  
+  return tutorials.map(titleCase)
 }
+
+function titleCase(item){
+  return item
+  .split(' ')
+  .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+  .join(' ');
+}
+console.log(titleCased())
